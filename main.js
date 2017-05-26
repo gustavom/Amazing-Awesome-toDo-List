607c43d03c7ee252ed71e10df08465767821fd9e
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2017-05-26 11:58:56 -0300

    form new task style

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
 (function (win, doc) {
 
+    var app = (function taskController() {
+
+        var $newTaskForm = doc.querySelector('[data-js="new-task-form"]'),
+            $newTaskTitle = doc.querySelector('[data-js="new-task-title"]'),
+            $newTaskText = doc.querySelector('[data-js="new-task-text"]');
+
+        return {
+            init: function () {
+                this.verifyInputs();
+            },
+            verifyInputs: function verifyInputs() {
+                $newTaskTitle.addEventListener('blur', function () {
+                    if (this.value !== "") {
+                        this.parentNode.classList.add('active');
+                    } else {
+                        this.parentNode.classList.remove('active');
+                    }
+                }, false);
+
+                $newTaskText.addEventListener('blur', function () {
+                    if (this.value !== "") {
+                        this.parentNode.classList.add('active');
+                    } else {
+                        this.parentNode.classList.remove('active');
+                    }
+                }, false);
+            },
+        }
+    })();
+
+    app.init();
+
 })(window, document);
